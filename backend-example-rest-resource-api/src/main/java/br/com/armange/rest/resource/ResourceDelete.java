2d89faebe739bda971de7f3ce7ad111af59c6c9b
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-03-14 15:52:13 -0300

    Removing comments.

--- backend-example-rest-resource-api/src/main/java/br/com/armange/rest/resource/ResourceDelete.java
+++ backend-example-rest-resource-api/src/main/java/br/com/armange/rest/resource/ResourceDelete.java
@@ -2,11 +2,4 @@ package br.com.armange.rest.resource;
 
 public interface ResourceDelete {
 
-//    AbstractService<T> getService();
-//    
-//    @DELETE
-//    @Path("/{id}")
-//    default Response delete(@PathParam("id") final UUID id) throws Exception {
-//        return Response.ok().build();
-//    }
 }
