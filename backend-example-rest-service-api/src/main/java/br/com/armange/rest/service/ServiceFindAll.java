5e2d1ed013c83d62a75e59f098fca8e1f8a20532
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-03-19 09:54:48 -0300

    Data paging

--- backend-example-rest-service-api/src/main/java/br/com/armange/rest/service/ServiceFindAll.java
+++ backend-example-rest-service-api/src/main/java/br/com/armange/rest/service/ServiceFindAll.java
@@ -1,5 +1,17 @@
-package br.com.armange.rest.service;
-
-public interface ServiceFindAll {
-
-}
+//package br.com.armange.rest.service;
+//
+//import javax.ws.rs.core.Response;
+//
+//import br.com.armange.dao.Dao;
+//import br.com.armange.entity.Identifiable;
+//
+//public interface ServiceFindAll<T, I extends Identifiable<T>> {
+//
+//    Dao<T, I> getDao();
+//    
+//    default Response findAll(final I identifiable) {
+//        getDao().fin(identifiable);
+//        
+//        return Response.noContent().build();
+//    }
+//}
